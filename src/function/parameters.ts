926120103e53f88629c4cf64afd2c4a1f9851d76
“joruji-tsuchiya” <0w0.joruji7582@gmail.com>
2022-02-09 00:43:22 +0900

    method study

---
+++ src/function/parameters.ts
@@ -0,0 +1,29 @@
+// オプションパラメーターを持つ関数
+export const isUserSignedIn = (userId: string, username?: string): boolean => {
+  if (userId === 'ABC') {
+    console.log('Function parameters sample 1: User is signed in! Username is', username)
+    return true
+  } else {
+    console.log('Function parameters sample 2: User is not signed in! Username is.')
+    return false
+  }
+}
+
+// デフォルトパラメータを持つ関数
+export const isUserSignedIn2 = (userId: string, username = 'NO NAME'): boolean => {
+  if (userId === 'ABC') {
+    console.log('Function parameters sample 3: User is signed in! Username is', username)
+    return true
+  } else {
+    console.log('Function parameters sample 4: User is not signed in! Username is.')
+    return false
+  }
+}
+
+// レストパラメータを持つ関数
+export const sumProductsPrice = (...productsPrice: number[]): number => {
+  //受け取った配列を足し合わせる
+  return productsPrice.reduce((prevTotal: number, productsPrice: number) => {
+    return prevTotal + productsPrice
+  }, 0)
+}
--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,20 @@
 // world.sayHello(root)
 
 // 03.基本の型定義
-import { anySample, primitiveSample } from './basic'
+// import { anySample, primitiveSample } from './basic'
 
-primitiveSample()
-anySample()
+// primitiveSample()
+// anySample()
+
+// 04.基本の型定義
+import { logMessage } from './function/basic'
+import { isUserSignedIn, isUserSignedIn2, sumProductsPrice } from './function/parameters'
+
+logMessage('Hello TypeScript')
+
+isUserSignedIn('ABC', 'Torahack')
+isUserSignedIn('DEF')
+isUserSignedIn2('ABC')
+
+const sum = sumProductsPrice(100, 200, 300, 400, 500)
+console.log('Function parameters sample 5:', sum)
